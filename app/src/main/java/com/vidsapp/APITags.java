2934be44d90aa7dbe9971cf35822d4708f51fe7c
atul-nanodegree <atul6883@gmail.com>
2016-11-26 13:28:51 +0530

    Adding Constant File
    Adding Constant File

---
+++ app/src/main/java/com/vidsapp/APITags.java
@@ -0,0 +1,14 @@
+package com.vidsapp;
+
+/**
+ * Created by atul
+ */
+public class APITags {
+
+    public static final String ID = "id";
+
+    public static final String NAME = "name";
+
+    public static final String DESCRIPTION = "description";
+
+}
